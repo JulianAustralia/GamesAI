8ebbf244bf2991f8d6fe04ce809a50d728898184
Julian Watson <contactjulianw@gmail.com>
2015-09-03 12:16:21 +1000

    Add FSM class and FS interface

---
+++ SimpsonsGame/Assets/Scripts/FiniteState.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(FiniteStateMachine))]
+public class FiniteState : MonoBehaviour {
+
+	public abstract FiniteState update();
+}
---
+++ SimpsonsGame/Assets/Scripts/FiniteStateMachine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FiniteStateMachine : MonoBehaviour {
+	
+	public FiniteState initialState;
+
+	private FiniteState _currentState;
+
+	void Awake () {
+	
+		_currentState = initialState;
+	}
+
+	void Update () {
+	
+		_currentState = _currentState.update();
+	}
+}
