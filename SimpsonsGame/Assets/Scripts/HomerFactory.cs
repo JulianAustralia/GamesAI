4e8e2f4f8564451e614ea9f246019dd62df637c9
Julian Watson <contactjulianw@gmail.com>
2015-09-03 15:29:25 +1000

    Programmatically generating the Homers

---
+++ SimpsonsGame/Assets/Scripts/HomerFactory.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomerFactory : MonoBehaviour {
+
+	public int numberOfHomers;
+	public GameObject[] homers;
+
+	void Start () {
+	
+		GameObject homer = GameObject.Find("Homer");
+		GameObject dome = GameObject.Find("Dome");
+
+		float radX = dome.transform.localScale.x / 2f;
+		float minX = dome.transform.position.x - radX;
+		float maxX = dome.transform.position.x + radX;
+
+		float radZ = dome.transform.localScale.z / 2f;
+		float minZ = dome.transform.position.z - radZ;
+		float maxZ = dome.transform.position.z + radZ;
+
+		for (int i = 0; i < numberOfHomers; ++i) {
+
+			Instantiate(
+				homer,
+				new Vector3(
+					UnityEngine.Random.Range(minX, maxX),
+					homer.transform.position.y,
+					UnityEngine.Random.Range(minZ, maxZ)
+				),
+				Quaternion.identity
+			);
+		}
+
+		// We could generate one less homer and randomly position this one
+		// But this way is cleaner and this function will only be called once
+		// per game so the performance hit is negligible
+		GameObject.Destroy(homer);
+	}
+}
