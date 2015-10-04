6852f0d63c1cfcc366d5ab0d143777bab48260f5
Julian Watson <contactjulianw@gmail.com>
2015-10-04 16:10:06 +1100

    Added Moe, Burns, and 4 safezones

---
+++ SimpsonsGame/Assets/Scripts/SafeZone.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SafeZone : MonoBehaviour {
+
+	public Enemy hunter;
+
+	void Start () {
+
+		Vector3 newPosition = new Vector3(
+			transform.position.x,
+			hunter.transform.position.y,
+			transform.position.z
+		);
+
+		Vector3 newRotation = new Vector3 (
+			-transform.position.x,
+			hunter.transform.position.y,
+			-transform.position.z
+		);
+
+		hunter.transform.position = newPosition;
+
+		hunter.transform.rotation = Quaternion.LookRotation(newRotation);
+	}
+
+	void Update () {
+	
+
+	}
+}
